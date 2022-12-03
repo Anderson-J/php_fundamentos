2b7829ecfad01d3389569bfeea1fa4b7a7ba81de
Anderson <anderaoliv@gmail.com>
2022-12-03 12:47:03 -0300

    Projeto de fundamentos PHP

---
+++ exibicao/principal.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Projeto PHP Fundamentos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body>
+    <nav class="navbar bg-light">
+      <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">Pricipal</span>
+      </div>
+    </nav>
+    <div class="container">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Nome do Curso</th>
+            <th scope="col">Carga Horária</th>
+            <th scope="col">Versão da ferramenta</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach(cursos() as $curso) : ?>
+            <?php 
+              if (!$curso["status"]) {
+                continue;
+              }
+            ?>
+            <tr>
+              <th scope="row"><?= $curso["nome_curso"] ?></th>
+              <td><?= carga_horaria($curso["carga_horaria"]) ?></td>
+              <td><?= $curso["versao_ferramenta"] ?></td>
+            </tr>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
